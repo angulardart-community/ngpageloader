70894295b648a04e9747bc7a034e0a4693285620
Gavin Zhao <git@gzgz.dev>
2022-01-28 16:18:36 -0500

    Add CI and fix missing tags
    Signed-off-by: Gavin Zhao <git@gzgz.dev>

--- test/src_null_safety/debug.dart
+++ test/src_null_safety/debug.dart
@@ -13,8 +13,6 @@
 
 import 'package:ngpageloader/debug.dart';
 import 'package:ngpageloader/pageloader.dart';
-import 'package:ngpageloader/src/debug/performance/profiler_listener.dart';
-import 'package:ngpageloader/src/debug/performance/timer_duration.dart';
 import 'package:test/test.dart';
 
 import 'shared_page_objects.dart';
